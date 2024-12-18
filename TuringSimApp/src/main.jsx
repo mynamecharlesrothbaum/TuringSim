62dbc2a6149bbf1a4d4ecae52ae793d391fc9352
Charles Rothbaum <33434729+mynamecharlesrothbaum@users.noreply.github.com>
2024-12-18 09:27:01 -0700

    server app

--- TuringSimApp/src/main.jsx
+++ TuringSimApp/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// Create the root and render the App component wrapped with BrowserRouter
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
